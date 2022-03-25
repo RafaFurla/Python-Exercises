8515c9f3b8419dd13b1b7510fa30bfbfc5c5363b
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-03-25 14:01:38 -0300

    ex005 - OOP

---
+++ Exercises in python solved with OOP method/ex005  - Predecessor and Successor.py
@@ -0,0 +1,35 @@
+class PHRASE:
+    def __init__(self, message, color='blue'):
+        self.message = str(message)
+        self.color = str(color)
+        if self.color == 'red':
+            self.message = '\033[31m' + self.message + '\033[m'
+        elif self.color == 'green':
+            self.message = '\033[32m' + self.message + '\033[m'
+        elif self.color == 'yellow':
+            self.message = '\033[33m' + self.message + '\033[m'
+        elif self.color == 'violet':
+            self.message = '\033[35m' + self.message + '\033[m'
+        elif self.color == 'blue':
+            self.message = '\033[36m' + self.message + '\033[m'
+
+    def prt(self):
+        return f"\033[35m{'#' * len(self.message)}\033[m\n{self.message:^{len(self.message) + 8}}\n\033[35m{'#' * len(self.message)}\033[m"
+
+
+class PREDSUC:
+    def __init__(self, value):
+        self.value = int(value)
+
+    def pred(self):
+        return self.value - 1
+
+    def suc(self):
+        return self.value + 1
+
+
+msg = PHRASE('An Script that print the predecessor and the successor of the input number', 'blue')
+print(msg.prt())
+num = PREDSUC(input('Input a Integer Number: '))
+print(f"The predecessor of number {num.value} is the number {num.pred()} and the sucessor is number {num.suc()}")
+

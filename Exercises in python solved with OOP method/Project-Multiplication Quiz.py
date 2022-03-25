c709e006cbdc8f559dcece02ed0b7996b2359858
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-03-25 18:26:13 -0300

    Project Multiplication Quiz - OOP

---
+++ Exercises in python solved with OOP method/Project-Multiplication Quiz.py
@@ -0,0 +1,22 @@
+from random import randint
+import time
+
+
+
+
+
+
+correct = incorrect = 0
+input('You have 10 seconds after pressing enter to answer as many questions as you can! ')
+now = time.time()
+future = now + 10
+for c in range(0, 50):
+    while time.time() < future:
+        n1 = randint(0, 10)
+        n2 = randint(0, 10)
+        r = int(input(f'How much is {n1} x {n2}? '))
+        if r == n1 * n2:
+            correct += 1
+        else:
+            incorrect += 1
+print(f'You aswering {correct} questions correctly and {incorrect} questions incorrectly!')

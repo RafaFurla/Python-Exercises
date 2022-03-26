08ff82c7cc8fa40e05fa7c23f07e50846944f418
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-03-26 00:51:17 -0300

    Project Multiplication Quiz - OOP

--- Exercises in python solved with OOP method/Project-Multiplication Quiz.py
+++ Exercises in python solved with OOP method/Project-Multiplication Quiz.py
@@ -1,20 +1,19 @@
 from random import randint
 import time
-
-
-
-
-correct = incorrect = 0
-input('You have 10 seconds after pressing enter to answer as many questions as you can! ')
-now = time.time()
-future = now + 10
-for c in range(0, 50):
-    while time.time() < future:
-        n1 = randint(0, 10)
-        n2 = randint(0, 10)
-        r = int(input(f'How much is {n1} x {n2}? '))
-        if r == n1 * n2:
-            correct += 1
-        else:
-            incorrect += 1
+input('After pressing "ENTER" you have 10 seconds to answer as many questions as you can! ')
+correct = incorrect = int(0)
+t0 = time.monotonic_ns()
+t = time.monotonic_ns()
+while ((t - t0)/1000000000) < 10:
+    if ((t - t0)/1000000000) == 0:
+        t0 = time.monotonic_ns()
+    n1 = randint(0, 10)
+    n2 = randint(0, 10)
+    r = int(input(f'How much is {n1} x {n2}? '))
+    t = time.monotonic_ns()
+    if r == n1 * n2:
+        correct += 1
+    else:
+        incorrect += 1
 print(f'You aswering {correct} questions correctly and {incorrect} questions incorrectly!')
+

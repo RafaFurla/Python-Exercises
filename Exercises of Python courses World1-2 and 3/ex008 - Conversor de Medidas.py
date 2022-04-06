eecd12caf48d5ec90eeb9a41e24a5a5854a3ca0c
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-04-05 23:50:55 -0300

    translate

--- Exercises of Python courses World1-2 and 3/ex008 - Conversor de Medidas.py
+++ Exercises of Python courses World1-2 and 3/ex008 - Conversor de Medidas.py
@@ -1,11 +1,11 @@
 color = {'clean': '\033[m', 'red': '\033[31m', 'green': '\033[32m'}
 print(color['red'], '=+=' * 21, color['clean'])
-print(' O programa converte uma medida em metros para as outras escalas')
+print(' The script convert one measure in meters to other magnitudes')
 print(color['red'], '=+=' * 21, color['clean'])
-n = float(input('Escreva uma medida em metros: '))
-print('{}{}{} metro(s) é igual a:\n{}{}{} quilômetro(s)\n{}{}{} hectômetro(s)'
-      '\n{}{}{} decâmetro(s)\n{}{}{} decimetro(s)\n{}{}{} centímetro(s)'
-      '\n{}{}{} milímetro(s)'
+n = float(input('Input a measure in meters: '))
+print('{}{}{} meter(s) is equal to:\n{}{}{} kilometer(s)\n{}{}{} hectometer(s)'
+      '\n{}{}{} dekameter(s)\n{}{}{} decimeter(s)\n{}{}{} centimeter(s)'
+      '\n{}{}{} millimeter(s)'
       .format(color['green'], n, color['clean'],
               color['red'], n/1000, color['clean'],
               color['red'], n/100, color['clean'],

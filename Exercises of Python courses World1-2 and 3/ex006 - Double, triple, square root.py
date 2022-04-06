4bf34580925083b93b8da78e88982ced66079ef8
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-04-05 23:44:28 -0300

    requirements.txt

--- Exercises of Python courses World1-2 and 3/ex006 - Double, triple, square root.py
+++ Exercises of Python courses World1-2 and 3/ex006 - Double, triple, square root.py
@@ -1,9 +1,9 @@
 color = {'green': '\033[32m', 'blue': '\033[36m', 'red': '\033[31m', 'purple': '\033[35m', 'clean': '\033[m'}
 print('\033[30;47m=+=\033[m' * 26)
-print('\033[30;43mPrograma que descobre o dobro, o triplo e a raíz quadrada do número digitado  \033[m')
+print('\033[30;43mScript that calculates the double, triple and square root of the input number  \033[m')
 print('\033[30;47m=+=\033[m' * 26)
-n = int(input('Escreva um número inteiro: '))
-print('O dobro de {}{}{} é {}{}{}, o triplo é {}{}{} e a raiz quadrada é {}{:.3}{}'
+n = int(input('Type a integer number: '))
+print('The double of {}{}{} is {}{}{}, the triple is {}{}{} and the square root is {}{:.3}{}'
       .format(color['green'], n, color['clean'],
               color['blue'], 2 * n, color['clean'],
               color['red'], 3 * n, color['clean'],

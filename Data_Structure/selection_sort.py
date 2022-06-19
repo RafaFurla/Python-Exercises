8c37ac5b13a28a5f22babfeac0d4b60f90c5aff7
Rafael Cardozo Furlanetto <99516189+RafaFurla@users.noreply.github.com>
2022-06-19 10:18:39 -0300

    Data Structure exercises

--- Data_Structure/selection_sort.py
+++ Data_Structure/selection_sort.py
@@ -1,19 +1,18 @@
-def sort(list):
+def sort(list_):
     """
-    This function is a selection sort algorithm and it will put a list in numerical order.
-    :param list: a list
+    This function is a selection sort algorithm. It will put a list in numerical order.
+    :param list_: a list
     :return: a list ordered by numerial order.
     """
 
-    for minimum in range(0, len(list)):
-        for c in range(minimum + 1, len(list)):
-            if list[c] < list[minimum]:
-                temporary = list[minimum]
-                list[minimum] = list[c]
-                list[c] = temporary
-    return list
+    for minimum in range(0, len(list_)):
+        for c in range(minimum + 1, len(list_)):
+            if list_[c] < list_[minimum]:
+                temporary = list_[minimum]
+                list_[minimum] = list_[c]
+                list_[c] = temporary
+    return list_
 
 
 numbers = [30, 18, 70, 100, 20, 5, 10, 50, 8, 14, 47]
 print(sort(numbers))
-print(numbers)

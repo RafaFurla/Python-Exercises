10275467689d8c985b1103b2cf2ea037668294da
RafaFurla <rafafurla@yahoo.com.br>
2022-11-13 19:30:21 +0100

    Exercise about math library

---
+++ Revision/5-math_library.py
@@ -0,0 +1,16 @@
+import math
+a = 0
+b = math.pi/4
+c = math.pi/2
+print(f"a = {a} rad, b = {b} rad, c = {c} rad")
+print(f"sin(a)={math.sin(a)}\nsin(b)={math.sin(b)}\nsin(c)={math.sin(c)}")
+print(f"sin(b) ceil = {math.ceil(math.sin(b))}")
+print(f"sin(b) floor = {math.floor(math.sin(b))}")
+print(f"sub(b) trunc = {math.trunc(math.sin(b))}")
+print(f"2^4 = {math.pow(2, 4)}")
+print(f"square(2^4) = {math.sqrt(math.pow(2, 4))}")
+print(f"5! = {math.factorial(5)}")
+print(f"hypotenuse triangle sides 3 and 4 = {math.hypot(3, 4)}")
+print(f"cosine(a) = {math.cos(a)}")
+print(f"tangent(b) = {math.tan(b)}")
+

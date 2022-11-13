407fe223fcf463274e7b6d1fc5d8a0b53561761e
RafaFurla <rafafurla@yahoo.com.br>
2022-11-13 21:07:36 +0100

    Tuple exercise

---
+++ Revision/9-tuples.py
@@ -0,0 +1,11 @@
+a = ("Hamburguer", "Fries", "Coca", "Desert")
+print(f"Our tuple is: {a}")
+for c in a:
+    print(c)
+print(a[2])
+print(a[0:2])
+print(a[1:])
+print(a[-1])
+print(f"The len of the tuple is: {len(a)}")
+del (a)
+

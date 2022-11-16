58bb4e1ca3f2eb870a71a7e927b846a87789d307
RafaFurla <rafafurla@yahoo.com.br>
2022-11-16 17:59:10 +0100

    Exercise about modules

---
+++ Revision/13-modules.py
@@ -0,0 +1,7 @@
+import modules
+
+sum = modules.sum(5, 6)
+sub = modules.subtraction(2, 8)
+mult = modules.multiply(7, 6)
+print(f"5 + 6 = {sum}\n8 - 2 = {sub}\n7 * 6 = {mult}")
+
---
+++ Revision/modules.py
@@ -0,0 +1,21 @@
+def sum(a=0, b=0):
+    """Sum between two inputted numbers"""
+    s = a + b
+    return s
+
+
+def subtraction(a=0, b=0):
+    """Subtraction between two inputted numbers"""
+    if a >= b:
+        s = a - b
+        return s
+    else:
+        s = b - a 
+        return s
+
+
+def multiply (a=1, b=1):
+    """This function multiply the two inputted numbers"""
+    m = a * b
+    return m
+

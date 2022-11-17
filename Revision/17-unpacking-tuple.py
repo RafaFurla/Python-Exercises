7442d9eec735206551703d4a9bcad3c4168132a1
RafaFurla <rafafurla@yahoo.com.br>
2022-11-17 18:28:15 +0100

    Exercise about unpacking tuples

---
+++ Revision/17-unpacking-tuple.py
@@ -0,0 +1,4 @@
+toll=("cashier-1", "cashier-2", "cashier-3", "cashier-4", "cashier-5", "cashier-6", "cashier-7")
+car1, car2, car3, car4, *car5 = toll
+print(f"The fifth car can choose the cashiers: {car5}")
+
